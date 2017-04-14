432f36af7071c0f1dbe31ea9ee978e01b34be2f7
Maxwell Sayles <maxwellsayles@gmail.com>
2017-04-13 21:56:09 -0700

    support for dry-run and not

--- SyncFiles.hs
+++ SyncFiles.hs
@@ -24,42 +24,62 @@ listFiles = listContents >=> filterM (\f -> isRegularFile <$> getFileStatus f)
 listDirs :: FilePath -> IO [FilePath]
 listDirs = listContents >=> filterM (\f -> isDirectory <$> getFileStatus f)
 
+cpFile :: Env -> FilePath -> IO ()
+cpFile (Env {..}) path = do
+  putStrLn $ ("cp " ++) $ srcPath </> path
+  when (not dryRun) $ do
+    copyFileWithMetadata (srcPath </> path) (dstPath </> path)
+
+rmFile :: Env -> FilePath -> IO ()
+rmFile (Env {..}) path = do
+  let path' = dstPath </> path
+  putStrLn $ "rm " ++ path'
+  when (not dryRun) $ removeFile path'
+
+mkDir :: Env -> FilePath -> IO ()
+mkDir (Env {..}) path = do
+  let path' = dstPath </> path
+  putStrLn $ "mkdir " ++ path'
+  when (not dryRun) $ createDirectoryIfMissing False path'
+
+rmDir :: Env -> FilePath -> IO ()
+rmDir (Env {..}) path = do
+  let path' = dstPath </> path
+  putStrLn $ "rm -rf " ++ path'
+  when (not dryRun) $ removePathForcibly path'
+
 syncFiles :: Env -> FilePath -> IO ()
-syncFiles (Env {..}) path = do
+syncFiles env@(Env {..}) path = do
   srcFiles <- S.fromList <$> (withCurrentDirectory srcPath $ listFiles path)
   dstFiles <- S.fromList <$> (withCurrentDirectory dstPath $ listFiles path)
 
   -- cp missing files to dst
   -- TODO: Grab metadata for src + dst before taking difference
   let cpFiles = srcFiles `S.difference` dstFiles
-  mapM_ (putStrLn . ("cp " ++)) $ S.map (srcPath </>) cpFiles
-  -- TODO: cp with time, owner, whatever
+  mapM_ (cpFile env) cpFiles
 
   -- rm extra files in dst
   let rmFiles = dstFiles `S.difference` srcFiles
-  mapM_ (putStrLn . ("rm " ++)) $ S.map (dstPath </>) rmFiles
-  -- TODO: rm
+  mapM_ (rmFile env) rmFiles
   
 syncDir :: Env -> FilePath -> IO ()
 syncDir env@(Env {..}) path = do
+  -- cp appropriate files from src to dst
   syncFiles env path
   
   srcDirs <- S.fromList <$> (withCurrentDirectory srcPath $ listDirs path)
   dstDirs <- S.fromList <$> (withCurrentDirectory dstPath $ listDirs path)
 
-  -- TODO: Create any missing dirs in dst
-  
-  --createDirectoryIfMissing False $ dst </> path
+  -- Create any missing dirs in dst
+  mapM_ (mkDir env) $ srcDirs `S.difference` dstDirs
 
   -- Iterate over all src dirs
   mapM_ (syncDir env) srcDirs
 
   -- rmdir extra dst dirs
-  let rmDirs = S.map (dstPath </>) $ dstDirs `S.difference` srcDirs
-  mapM_ (putStrLn . ("rm -rf " ++)) rmDirs
-  -- TODO: rmdir
+  mapM_ (rmDir env) $ dstDirs `S.difference` srcDirs
 
 main :: IO ()
 main = do
-  let env = Env "/home/max/tmp" "/home/max/Downloads" True
-  syncDir env "."
+  let env = Env "/home/max/" "/home/max/tmp2" False
+  syncDir env "tmp"
